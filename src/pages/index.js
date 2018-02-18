30813655bc57e17c8147b2f0802ed2779afc047e
Matt Davis <matt.davis@mediacurrent.com>
2018-02-18 12:04:29 -0800

    Updating index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
 
 const IndexPage = () => (
   <div>
     <h1>Welcome</h1>
     <p>This is a brand new site where I play around with decentralized web protocols and other fun stuff.</p>
-    <p>I built it using <a href="">Gatsby</a> and React.</p>
-    <p>Insert circa-mid-90's Under Construction animated gif here.</p>
+    <p>I built it using <a href="https://www.gatsbyjs.org" target="_blank">Gatsby</a> and React.</p>
+    <p>Insert circa-mid-90\'s Under Construction animated gif here.</p>
     <p>There are several ways you can view this site. Using the traditional Internet you can visit its Github Pages home <a href="https://mrjmd.github.io/cyuio/">here</a>.</p>
     <p>This site is also available for viewing via <a href="https://ipfs.io" target="_blank">IPFS</a> by going <a href="https://gateway.ipfs.io/ipns/QmaLp9R3mX38RpFH2hiud6r3FbTGup5mDfbFuNCahBAXAw/">here</a>.</p>
-    <p>If you are using Beaker Browser you can also access a p2p version of this site on the <a href="">DAT Protocol</a> by clicking <a href="dat://196c3058aaa82d964b6b4084f0076b3e4a5c45809cebc80f8a814cbce30af14a/">here</a>.</p>
+    <p>If you are using Beaker Browser you can also access a p2p version of this site on the <a href="https://datproject.org/" target="_blank">DAT Protocol</a> by clicking <a href="dat://196c3058aaa82d964b6b4084f0076b3e4a5c45809cebc80f8a814cbce30af14a/">here</a>.</p>
   </div>
-)
+);
 
-export default IndexPage
+export default IndexPage;
